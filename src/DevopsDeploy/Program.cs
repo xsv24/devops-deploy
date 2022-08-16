f28fdeff22ac66939bf6551bda14db8e3795635d
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-08-16 21:44:19 +1200

    Use json-pact to validate required schema.

--- src/DevopsDeploy/Program.cs
+++ src/DevopsDeploy/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Text.Json;
-using DevopsDeploy.Models;
+﻿using DevopsDeploy.Models;
+using JsonPact;
+using JsonPact.NewtonSoft;
 using Environment = DevopsDeploy.Models.Environment;
 
-var deployments = JsonSerializer.Deserialize<Deployment[]>(File.ReadAllText("./Json/Deployments.json"))
+var pact = JsonPacts.Default(JsonPactCase.Pascal).IntoJsonPact();
+
+var deployments = pact.Deserialize<Deployment[]>(File.ReadAllText("./Json/Deployments.json"))
     ?? throw new ArgumentException($"Expected valid a list of {nameof(Deployment)}'s.");
 
-var environments = JsonSerializer.Deserialize<Environment[]>(File.ReadAllText("./Json/Environments.json"))
+var environments = pact.Deserialize<Environment[]>(File.ReadAllText("./Json/Environments.json"))
     ?? throw new ArgumentException($"Expected valid a list of {nameof(Environment)}'s.");
 
-var projects = JsonSerializer.Deserialize<Project[]>(File.ReadAllText("./Json/Projects.json"))
+var projects = pact.Deserialize<Project[]>(File.ReadAllText("./Json/Projects.json"))
     ?? throw new ArgumentException($"Expected valid a list of {nameof(Project)}'s.");
 
-var releases = JsonSerializer.Deserialize<Release[]>(File.ReadAllText("./Json/Releases.json"))
+var releases = pact.Deserialize<Release[]>(File.ReadAllText("./Json/Releases.json"))
     ?? throw new ArgumentException($"Expected valid a list of {nameof(Release)}'s.");
 
 Console.WriteLine(deployments[0]);
