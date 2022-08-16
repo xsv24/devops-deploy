4db77f67b62ec24cf6a90e9e8a62fb696181a939
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-08-16 19:46:54 +1200

    Add inital load of files.

--- src/DevopsDeploy/Program.cs
+++ src/DevopsDeploy/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using DevopsDeploy.Models;
+using Environment = DevopsDeploy.Models.Environment;
+
+var deployments = JsonSerializer.Deserialize<Deployment[]>(File.ReadAllText("./Json/Deployments.json"))
+    ?? throw new ArgumentException($"Expected valid a list of {nameof(Deployment)}'s.");
+
+var environments = JsonSerializer.Deserialize<Environment[]>(File.ReadAllText("./Json/Environments.json"))
+    ?? throw new ArgumentException($"Expected valid a list of {nameof(Environment)}'s.");
+
+var projects = JsonSerializer.Deserialize<Project[]>(File.ReadAllText("./Json/Projects.json"))
+    ?? throw new ArgumentException($"Expected valid a list of {nameof(Project)}'s.");
+
+var releases = JsonSerializer.Deserialize<Release[]>(File.ReadAllText("./Json/Releases.json"))
+    ?? throw new ArgumentException($"Expected valid a list of {nameof(Release)}'s.");
+
+Console.WriteLine(deployments[0]);
+Console.WriteLine(environments[0]);
+Console.WriteLine(projects[0]);
+Console.WriteLine(releases[0]);
