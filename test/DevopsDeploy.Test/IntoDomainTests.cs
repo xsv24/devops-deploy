d137b5be268dde320825485eb8fde612eb4148f4
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-08-18 18:15:46 +1200

    Add IntoDomain tests.

---
+++ test/DevopsDeploy.Test/IntoDomainTests.cs
@@ -0,0 +1,167 @@
+using AutoFixture.Xunit2;
+using DevopsDeploy.Models;
+using FluentAssertions;
+using Environment = DevopsDeploy.Models.Environment;
+
+namespace DevopsDeploy.Test;
+
+public class IntoDomainTests
+{
+    [Theory, AutoData]
+    public void IntoDeploymentCollection_Groups(DeploymentData data)
+    {
+        data.IntoDeploymentCollection()
+            .Should().BeEmpty("As none of the id's match up");
+    }
+
+    [Theory, AutoData]
+    public void IntoDeploymentCollection_Does_Not_Error_On_Id_Miss_Matches(DeploymentData data)
+    {
+        data.IntoDeploymentCollection()
+            .Should().BeEmpty("As none of the id's match up");
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomainOrDefault_With_Required_Matches(
+        Deployment deployment,
+        Environment environment,
+        Release release,
+        Project project,
+        Dictionary<string, Environment> environments,
+        Dictionary<string, Release> releases,
+        Dictionary<string, Project> projects
+    )
+    {
+        environments.Add(deployment.EnvironmentId.Sanitise(), environment);
+        releases.Add(deployment.ReleaseId.Sanitise(), release with { ProjectId = project.Id });
+        projects.Add(project.Id.Sanitise(), project);
+
+        var deploy = deployment.IntoDomainOrDefault(
+            environments,
+            releases,
+            projects
+        );
+
+        deploy.Should().Be(new DeploymentDomain(
+            Id: deployment.Id,
+            DeployedAt: deployment.DeployedAt,
+            Release: new ReleaseDomain(release.Id, project, release.Version, release.Created),
+            Environment: environment
+         ));
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomainOrDefault_With_No_Matching_Environment_Returns_Null(
+        Deployment deployment,
+        Dictionary<string, Release> releases,
+        Dictionary<string, Project> projects
+    )
+    {
+        var deploy = deployment.IntoDomainOrDefault(
+            new Dictionary<string, Environment>(),
+            releases,
+            projects
+        );
+
+        deploy.Should().BeNull();
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomainOrDefault_With_No_Matching_Releases_Returns_Null(
+        Deployment deployment,
+        Environment environment,
+        Dictionary<string, Environment> environments,
+        Dictionary<string, Project> projects
+    )
+    {
+        environments.Add(deployment.EnvironmentId.Sanitise(), environment);
+
+        var deploy = deployment.IntoDomainOrDefault(
+            environments,
+            new Dictionary<string, Release>(),
+            projects
+        );
+
+        deploy.Should().BeNull();
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomainOrDefault_With_No_Matching_Project_Returns_Null(
+        Deployment deployment,
+        Environment environment,
+        Release release,
+        Dictionary<string, Environment> environments,
+        Dictionary<string, Release> releases
+    )
+    {
+        environments.Add(deployment.EnvironmentId.Sanitise(), environment);
+        releases.Add(deployment.ReleaseId.Sanitise(), release);
+
+        var deploy = deployment.IntoDomainOrDefault(
+            environments,
+            releases,
+            new Dictionary<string, Project>()
+        );
+
+        deploy.Should().BeNull();
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomain_With_No_Matching_Environment_Throws(
+        Deployment deployment,
+        Dictionary<string, Release> releases,
+        Dictionary<string, Project> projects
+    )
+    {
+        var act = () => deployment.IntoDomain(
+            new Dictionary<string, Environment>(),
+            releases,
+            projects
+        );
+
+        act.Should().ThrowExactly<NotFoundException>()
+            .WithMessage($"No match found for {nameof(Environment)} with identifier {deployment.EnvironmentId}");
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomain_With_No_Matching_Releases_Throws(
+        Deployment deployment,
+        Environment environment,
+        Dictionary<string, Environment> environments,
+        Dictionary<string, Project> projects
+    )
+    {
+        environments.Add(deployment.EnvironmentId.Sanitise(), environment);
+
+        var act = () => deployment.IntoDomain(
+            environments,
+            new Dictionary<string, Release>(),
+            projects
+        );
+
+        act.Should().ThrowExactly<NotFoundException>()
+            .WithMessage($"No match found for {nameof(Release)} with identifier {deployment.ReleaseId}");
+    }
+
+    [Theory, AutoData]
+    public void Deployment_IntoDomain_With_No_Matching_Project_Throws(
+        Deployment deployment,
+        Environment environment,
+        Release release,
+        Dictionary<string, Environment> environments,
+        Dictionary<string, Release> releases
+    )
+    {
+        environments.Add(deployment.EnvironmentId.Sanitise(), environment);
+        releases.Add(deployment.ReleaseId.Sanitise(), release);
+
+        var act = () => deployment.IntoDomain(
+            environments,
+            releases,
+            new Dictionary<string, Project>()
+        );
+
+        act.Should().ThrowExactly<NotFoundException>()
+            .WithMessage($"No match found for {nameof(Project)} with identifier {release.ProjectId}");
+    }
+}
